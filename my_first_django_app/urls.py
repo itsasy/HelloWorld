51e2fda27defc6063f654bdaea641f3d6f9bc47e
itsasy <45985585+itsasy@users.noreply.github.com>
2021-01-06 20:38:04 -0500

    Finish HelloWorld

--- my_first_django_app/urls.py
+++ my_first_django_app/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from helloWorld.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello-world/', index)
 ]
