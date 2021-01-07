bad03459a502fa9afc4a61786f03ccbc37b1d611
itsasy <45985585+itsasy@users.noreply.github.com>
2021-01-06 20:32:16 -0500

    Create App HelloWord

--- my_first_django_app/settings.py
+++ my_first_django_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+#bq1$pw(xsqp1yziydn+lwkrn9di)xfom&m07$vc*choz8v-#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'helloWorld'
 ]
 
 MIDDLEWARE = [
